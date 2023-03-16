25731893737099bb5580706899569278fe9b5eb9
Aleksey <lexanik10@list.ru>
2023-03-16 09:30:40 +0300

    Независимость контента

---
+++ pages/product_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def click_button_add_to_cart(self):
+        button_cart = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_CART)
+        button_cart.click()
+
+    def should_be_button_add_to_cart(self):
+        assert self.is_element_present(*ProductPageLocators.BUTTON_ADD_TO_CART), "Button add to cart is not presented"
+
+    def should_match_product_names(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        print(f'Product name is {product_name}')
+        product_name_check = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_CHECK).text
+        print(f'Product name check is {product_name_check}')
+        assert product_name == product_name_check, "Product names not match"
--- test_main_page.py
+++ test_main_page.py
@@ -5,6 +5,7 @@ from .pages.login_page import LoginPage
 
 
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser,link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
@@ -41,6 +42,7 @@ def test_guest_should_be_login_url(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
+
 
 
 
